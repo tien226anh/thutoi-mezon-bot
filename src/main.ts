93a2d104c172d2473c4874ea8f735253e66f1b60
anhnt <tien226anh@gmail.com>
2025-05-21 00:13:54 +0700

    "feat/config-base port config"

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.APP_PORT ?? 3000);
+  logger.log(`App is running on port ${process.env.APP_PORT ?? 3000}`);
 }
 bootstrap();
